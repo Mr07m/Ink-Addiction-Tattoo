9615cc7a0c404fface93a146014a420ca0d6e3d7
Mr07m <mrgithub07@gmail.com>
2024-10-17 19:33:10 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let viewMore = document.getElementById("view-more");
+// let viewMoreBtn = document.getElementById("view-more-btn");
+
+// viewMoreBtn.addEventListener("click", () => {
+//   viewMore.style.display = "block";
+// });
+
+
